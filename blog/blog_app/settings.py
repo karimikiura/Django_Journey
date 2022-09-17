aa580af6ee6e5806911a607f3019a0fc270f391b
karimikiura <nicholaskarimi.dev@gmail.com>
2022-09-17 10:05:51 +0300

    user account

--- blog/blog_app/settings.py
+++ blog/blog_app/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog',
+    'accounts',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,4 +125,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
